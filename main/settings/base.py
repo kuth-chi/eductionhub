53cde9f5f2444c908d7d04d8ad7ac2d86b251462
kuth-chi <69514175+ezzhop@users.noreply.github.com>
2024-07-22 23:47:16 +0700

    Configure database

--- main/settings/base.py
+++ main/settings/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 print("DEBUG: ", DEBUG)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
--- main/settings/production.py
+++ main/settings/production.py
@@ -1,14 +1,16 @@
 import os
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ["*"]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 # CORS HEADER
-# CORS_ALLOWED_ORIGINS = []
-# print("CORS_ALLOWED_ORIGINS: ", CORS_ALLOWED_ORIGINS)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8100",
+    "http://127.0.0.1:8100",
+]
 
 TEMPLATES = [
     {
@@ -28,28 +30,10 @@ TEMPLATES = [
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-# hostname = os.getenv('DBHOST')
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DBNAME'),
-#         'HOST': os.getenv('DBHOST'),
-#         'USER': os.getenv('DBUSER'),
-#         'PASSWORD': os.getenv('DBPASS') 
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Cache Settings
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "rediss://:oHh1CWAAMBk2m3pTOc0NjZRXmzum38vr3AzCaLm7AM0=@edu-cache.redis.cache.windows.net:6380/0",
     }
 }
 
@@ -72,11 +56,3 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 ]
-
-# Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
