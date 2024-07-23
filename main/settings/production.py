de046a40ff2ee2bcca2aeb035a72f6f70e87f1da
kuth-chi <kuthchi@outlook.com>
2024-07-23 23:10:55 +0700

    Added Secret Key

--- main/settings/production.py
+++ main/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 # CORS HEADER
 CORS_ALLOWED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"],]
