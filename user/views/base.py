4607fe5f763fc26e5c1af59611362068c7a85aaf
kuth-chi <kuthchi@outlook.com>
2024-07-27 11:53:08 +0700

    Update home page layout

--- user/views/base.py
+++ user/views/base.py
@@ -1,16 +1,22 @@
 """
 Base function to handle page
 """
+import requests
 from django.shortcuts import render
 
+UNSPLASH = requests.get("https://picsum.photos/v2/list?page=2&limit=54", timeout=10).json()
 
 def index(request):
     '''
     Function to render home page
     '''
     template_name = "pages/home.html"
+    
+    
+    image_urls = UNSPLASH    
     context = {
         "page_title": "Home",
-        "header_title": "Home"
+        "header_title": "Home",
+        'images': image_urls
     }
     return render(request, template_name, context)
