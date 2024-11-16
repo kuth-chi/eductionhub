184cdeb48dfcdb31e2ea4d95d709afb8bbc2afb6
Kuth <kuthchi@outlook.com>
2024-11-16 15:14:28 +0700

    Update Settings.py

--- main/production.py
+++ main/production.py
@@ -5,7 +5,7 @@ from main.settings import BASE_DIR
 from azure.identity import DefaultAzureCredential
 
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else ["*"]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # SECURITY WARNING: don't run with debug turned on in production!
