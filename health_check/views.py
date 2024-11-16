23b6c7ae4174fd6f4ccc30dc8a706202bcd08b42
Kuth <kuthchi@outlook.com>
2024-11-16 21:01:14 +0700

    Update logic check health

--- health_check/views.py
+++ health_check/views.py
@@ -1,16 +1,35 @@
+import os
+import base64
 from django.http import JsonResponse
-from django.shortcuts import render
+from hashlib import sha256
+from django.http import JsonResponse, HttpResponseForbidden
 
-# Create your views here.
+
+def header_matches_env_var(header_value):
+    """
+    Returns true if SHA256 of header_value matches WEBSITE_AUTH_ENCRYPTION_KEY.
+
+    :param header_value: Value of the x-ms-auth-internal-token header.
+    """
+    env_var = os.getenv('WEBSITE_AUTH_ENCRYPTION_KEY')
+    if not env_var or not header_value:
+        return False
+
+    hash = base64.b64encode(
+        sha256(env_var.encode('utf-8')).digest()).decode('utf-8')
+    return hash == header_value
 
 
 def health_check(request):
-    ''' This is a helper function to check the health of applications '''
+    auth_header = request.headers.get('x-ms-auth-internal-token')
+    if not header_matches_env_var(auth_header):
+        return HttpResponseForbidden("Invalid token")
+
+    # Health check logic
     data = {
         "status": "healthy",
         "database": True
     }
-    
     try:
         from django.db import connections
         from django.db.utils import OperationalError
@@ -18,5 +37,5 @@ def health_check(request):
         db_conn.cursor()
     except OperationalError:
         data["database"] = False
-    
+
     return JsonResponse(data, status=200 if data["database"] else 500)
