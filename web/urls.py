413561fc03a1aca44dfec10af551a2d7909c375a
Kuth <69514175+kuth-chi@users.noreply.github.com>
2025-09-02 22:30:36 +0700

    modified:   static/css/style.css modified:   templates/_base.html modified:   templates/allauth/elements/provider.html modified:   templates/allauth/elements/provider_list.html modified:   templates/pages/home.html modified:   templates/pages/privacy.html modified:   templates/pages/terms-conditions.html modified:   templates/shared/_footer.html modified:   templates/shared/_navbar.html modified:   web/urls.py

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from .views import about_us, our_privacy, terms_conditions
 
 app_name = "web"
 
 urlpatterns = [
     path("about-us/", about_us, name="about_us"),
-    path("privacy-policy/", our_privacy, name="about_us"),
-    path("terms-conditions/", terms_conditions, name="about_us"),
+    path("privacy/", our_privacy, name="privacy_policy"),
+    path("terms-conditions/", terms_conditions, name="terms_conditions"),
 ]
